2f9014917a08c4f38e29e6aeb5d80579c9b55c9e
Sandra <sandracuadraruiz@fp.oceanoatlantico.org>
2021-05-03 13:55:29 +0200

    sandra

--- ATuSalud/Models/TablaDatosFisicos.cs
+++ ATuSalud/Models/TablaDatosFisicos.cs
@@ -11,7 +11,7 @@ namespace ATuSalud.Models
     {		
 		public int Id { get; set; }
 		
-		public string Nombre { get; set; }
+		//public string Nombre { get; set; }
 
 		[Column("Id_Paciente")]
 		public int? PacienteId { get; set; }
