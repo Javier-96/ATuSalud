ca31deec2b97f7ba514213d62ed56d1507282341
Andres <andresgutierrezclimente@fp.oceanoatlantico.org>
2021-05-07 09:47:01 +0200

    Query 6

---
+++ ATuSalud/ViewData/SqlQuery_6ViewData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.ViewData
+{
+    public class SqlQuery_6ViewData
+    {
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public int? numMedicamentos { get; set; }
+    }
+}
