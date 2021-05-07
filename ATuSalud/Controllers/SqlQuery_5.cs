b8ef822e4df71588ebeb0270d19f09ee15dd3dab
Victor <a@a.com>
2021-05-07 09:50:13 +0200

    Victor

---
+++ ATuSalud/Controllers/SqlQuery_5.cs
@@ -0,0 +1,38 @@
+﻿using ATuSalud.ViewData;
+using ConexionSQL.Models;
+using Demostracion;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.Controllers
+{
+    public class SqlQuery_5 : Controller
+    {
+        private readonly Contexto _context;
+        //Ahorrar algoritmo recorrido etc
+        private readonly ServicioSQL _sql;
+        public SqlQuery_5(Contexto context, ServicioSQL sql)
+        {
+            _context = context;
+            _sql = sql;
+        }
+        public IActionResult Index()
+        {
+            string sql = "SELECT nombre_medicamento FROM tablamedicamentos ORDER BY nombre_medicamento";
+            List<SqlQuery_5ViewData> lista =
+            _sql.EjecutarSQL<SqlQuery_5ViewData>(
+                    _context, sql,
+                    x => new SqlQuery_5ViewData()
+                    {
+
+                        nombre_medicamento = x.GetString(0),
+                   
+                    }
+                );
+            return View(lista);
+        }
+    }
+}
---
+++ ATuSalud/ViewData/SqlQuery_5ViewData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.ViewData
+{
+    public class SqlQuery_5ViewData
+    {
+        public string nombre_medicamento { get; set; }
+
+    }
+}
