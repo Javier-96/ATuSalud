89ab9b3ce3900ec33f158d175d09bea1295993ce
Sandra <sandracuadraruiz@fp.oceanoatlantico.org>
2021-05-07 09:49:35 +0200

    sandra

---
+++ ATuSalud/ViewData/SqlQuery_3ViewData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.ViewData
+{
+    public class SqlQuery_3ViewData
+    {
+        public string Codigo { get; set; }
+        public string Enfermedad { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
