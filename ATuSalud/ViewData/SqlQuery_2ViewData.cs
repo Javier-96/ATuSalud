f125caa3de3fb4fdcd162b136240f5dca85692eb
Nils <nilsschindlervelilla@fp.oceanoatlantico.org>
2021-05-07 09:48:57 +0200

    nils

---
+++ ATuSalud/ViewData/SqlQuery_2ViewData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.ViewData
+{
+    public class SqlQuery_2ViewData
+    {
+       public string apellido1 { get; set; }
+       public string apellido2 { get; set; }
+       public string nombre { get; set; }
+       public DateTime fecha_de_Nacimiento { get; set; }
+    }
+}
+
