19fa7635d825dcdf944a14ac7b5b19c7bf47afb4
Javier <jvivas1996@gmail.com>
2021-05-07 13:36:30 +0200

    Javier

---
+++ ATuSalud/Controllers/SqlQuery_8.cs
@@ -0,0 +1,55 @@
+﻿using ATuSalud.ViewData;
+using ConexionSQL.Models;
+using Demostracion;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.Controllers
+{
+    public class SqlQuery_8 : Controller
+    {
+        private readonly Contexto _context;
+        //Ahorrar algoritmo recorrido etc
+        private readonly ServicioSQL _sql;
+        public SqlQuery_8(Contexto context, ServicioSQL sql)
+        {
+            _context = context;
+            _sql = sql;
+        }
+        //Vamos a pasarlo por Get
+        public IActionResult Index(int? idPaciente)
+        {
+            string sql = "SELECT a.id,c.enfermedad,p.nombre " +
+                "FROM tablaantecedentes a INNER JOIN tablapaciente p " +
+                "ON(p.id = a.Id_paciente) INNER JOIN tablacodigociap c ON(c.id = a.id_ciap)" +
+                " WHERE 1=1 " +
+                (idPaciente != null ? " AND p.id = @id" : "");
+
+            MySqlParameter[] param =
+            {
+                new MySqlParameter("@id", idPaciente)
+            };
+
+            List<SqlQuery_8ViewData> lista =
+            _sql.EjecutarSQL<SqlQuery_8ViewData>(
+                    _context, sql,
+                    x => new SqlQuery_8ViewData()
+                    {
+                        id_antecedente = x.GetInt32(0),
+                        enfermedad = x.GetString(1),
+                        nombre = x.GetString(2)
+                    },param
+                );
+
+
+            ViewBag.Pac = new SelectList(_context.TablaPaciente, "Id", "Nombre", idPaciente);
+            
+            return View(lista);
+        }
+    }
+}
---
+++ ATuSalud/ViewData/SqlQuery_8Param.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.ViewData
+{
+    public class SqlQuery_8Param
+    {
+        public int? id { get; set; }
+    }
+}
