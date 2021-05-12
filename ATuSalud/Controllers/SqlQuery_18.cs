96f4ec38e9e4b12cbc24fd52c24f6161ce61ccba
Victor <a@a.com>
2021-05-12 14:32:48 +0200

    victor

---
+++ ATuSalud/Controllers/SqlQuery_18.cs
@@ -0,0 +1,40 @@
+﻿using ATuSalud.ViewData;
+using ConexionSQL.Models;
+using Demostracion;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.Controllers
+{
+    public class SqlQuery_18 : Controller
+    {
+        private readonly Contexto _context;
+        //Ahorrar algoritmo recorrido etc
+        private readonly ServicioSQL _sql;
+        public SqlQuery_18(Contexto context, ServicioSQL sql)
+        {
+            this._context = context;
+            this._sql = sql;
+        }
+
+        public IActionResult Index()
+        {
+            string sql = "SELECT p.nombre,c.codigo,c.Enfermedad,a.Familiar FROM tablapaciente p INNER JOIN tablaantecedentes a ON(p.id= a.Id_Paciente) INNER JOIN tablacodigociap c ON(c.id = a.id_ciap) WHERE c.Codigo LIKE 'P%'";
+            List<SqlQuery_18ViewData> lista =
+            _sql.EjecutarSQL<SqlQuery_18ViewData>(
+                    _context, sql,
+                    x => new SqlQuery_18ViewData()
+                    { 
+                        nombre = x.GetString(0),
+                        codigo = x.GetString(1),
+                        Enfermedad = x.GetString(2),
+                        Familiar = x.GetString(3)
+                    }
+                );
+            return View(lista);
+        }
+    }
+}
---
+++ ATuSalud/ViewData/SqlQuery_18ViewData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATuSalud.ViewData
+{
+    public class SqlQuery_18ViewData
+    {
+        public string nombre { get; set; }
+        public string codigo { get; set; }
+        public string Enfermedad { get; set; }
+        public string Familiar { get; set; }
+
+    }
+}
