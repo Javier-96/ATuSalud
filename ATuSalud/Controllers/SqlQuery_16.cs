2d4889c356e9bf260a3716598b99a7746fbbd102
Nils <nilsschindlervelilla@fp.oceanoatlantico.org>
2021-05-14 09:01:22 +0200

    Nils

--- ATuSalud/Controllers/SqlQuery_16.cs
+++ ATuSalud/Controllers/SqlQuery_16.cs
@@ -25,24 +25,27 @@ namespace ATuSalud.Controllers
        // [HttpPost]
         public IActionResult Index()
         {
-            string sql = " SELECT p.id,p.Nombre,p.Apellido1,p.Apellido2 " +
-                         " FROM tablapaciente p " +
-                          " WHERE id IN( " +
-                                 " SELECT tr.id_paciente " +
-                                 " FROM tablaincompatibilidades ti " +
-                                 " INNER JOIN tablarecetas tr ON(tr.id_medicamento = ti.id_medicamento) " +
-                                 " INNER JOIN tablarecetas tr2 ON(tr2.id_medicamento = ti.id_medicamento_incompatible) " +
-                                 " WHERE tr.id_paciente = tr2.id_paciente AND tr.dias_regenerado != 0 )";
+            string sql = " SELECT p.Nombre,p.Apellido1,p.Apellido2,tm.Nombre_medicamento,tm2.Nombre_medicamento " +
+                        " FROM tablaincompatibilidades ti " +
+                        " INNER JOIN tablarecetas tr ON(tr.id_medicamento = ti.id_medicamento) " +
+                        " INNER JOIN tablarecetas tr2 ON(tr2.id_medicamento = ti.id_medicamento_incompatible) " +
+                        " INNER JOIN tablapaciente p ON(tr2.id_paciente = p.id) " +
+                        " INNER JOIN tablamedicamentos tm ON(tr.id_medicamento = tm.id) " +
+                        " INNER JOIN tablamedicamentos tm2 ON(tr2.id_medicamento = tm2.id) " +
+                        " WHERE tr.id_paciente = tr2.id_paciente AND tr.dias_regenerado != 0 ";
+
 
             List<SqlQuery_16ViewData> lista =
             _sql.EjecutarSQL<SqlQuery_16ViewData>(
                     _context, sql,
                     x => new SqlQuery_16ViewData()
                     {
-                        id_paciente = x.GetInt32(0),
-                        nombre= x.GetString(1),
-                        apellido1 = x.GetString(2),
-                        apellido2 = x.GetString(3)
+                      
+                        nombre= x.GetString(0),
+                        apellido1 = x.GetString(1),
+                        apellido2 = x.GetString(2),
+                        medicamento1=x.GetString(3),
+                        medicamento2=x.GetString(4)
                     }
                 );
             return View(lista);
--- ATuSalud/ViewData/SqlQuery_16ViewData.cs
+++ ATuSalud/ViewData/SqlQuery_16ViewData.cs
@@ -7,10 +7,12 @@ namespace ATuSalud.ViewData
 {
     public class SqlQuery_16ViewData
     {
-        public int? id_paciente { get; set; }
+       
         public string nombre { get; set; }
         public string apellido1 { get; set; }
         public string apellido2 { get; set; }
+        public string medicamento1 { get; set; }
+        public string medicamento2 { get; set; }
 
     }
 }
